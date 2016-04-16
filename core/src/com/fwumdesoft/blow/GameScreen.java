6af8ec0b9f56104b74c77ce55bc239636d2c51f2
Ryan Goldstein <ryanisaacg@gmail.com>
2016-04-16 15:48:59 -0400

    Fixed bugs with render pipeline not being passed the spritebatch

--- core/src/com/fwumdesoft/blow/GameScreen.java
+++ core/src/com/fwumdesoft/blow/GameScreen.java
@@ -15,15 +15,15 @@ public class GameScreen extends ScreenAdapter {
 	final float WORLD_WIDTH = 1920, WORLD_HEIGHT = 1080;
 	
 	public GameScreen() {
-		render = new Renderer(batch);
 		batch = new SpriteBatch();
+		render = new Renderer(batch);
 		Camera camera = new OrthographicCamera();
 		Viewport viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
 		stage = new Stage(viewport, batch);
 	}
 	
 	public void update() {
-		stage.act();
+		//stage.act();
 	}
 	
 	@Override
