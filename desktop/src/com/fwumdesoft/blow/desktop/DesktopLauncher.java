9895e1aece9137f198e5a9e4b2853d540b9054dc
Ryan Goldstein <ryanisaacg@gmail.com>
2016-04-16 10:59:01 -0400

    Initial commit

---
+++ desktop/src/com/fwumdesoft/blow/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fwumdesoft.blow.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fwumdesoft.blow.BlowItUp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BlowItUp(), config);
+	}
+}
