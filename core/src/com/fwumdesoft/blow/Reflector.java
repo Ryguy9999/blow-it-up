65d0a425d3368219ad5077a341ef4b240fc96c4d
Ryan Goldstein <ryanisaacg@gmail.com>
2016-04-16 18:29:14 -0400

    Checked against all missiles for a reflect

--- core/src/com/fwumdesoft/blow/Reflector.java
+++ core/src/com/fwumdesoft/blow/Reflector.java
@@ -1,26 +1,45 @@
 package com.fwumdesoft.blow;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.Array;
+
 public class Reflector extends DrawingActor {
 
 	public float range;
 	public int lane;
-	
+
 	public Reflector(int lane) {
 		this(10, lane);
 	}
+
 	public Reflector(float range, int lane) {
-		super(null); //TODO Replace with reflector asset when done
+		super(null); // TODO Replace with reflector asset when done
 		this.range = range;
 		this.lane = lane;
 	}
-	
+
 	public void launchMissile() {
 		getStage().addActor(new Missile(10, 0, 0, lane));
 	}
-	
+
 	public void reflectMissile(Missile m) {
-		if(m.getRotation() != this.getRotation())
+		if (m.getRotation() != this.getRotation())
 			m.setRotation(m.getRotation() + 180);
+	}
+
+	public void reflectMissiles() {
+		Array<Actor> actors = getStage().getActors();
+		for (Actor actor : actors) {
+			if (actor instanceof Missile) {
+				Missile m = (Missile) actor;
+				if (m.lane != lane)
+					continue;
+				if (Vector2.dst(getX(), getY(), m.getX(), m.getY()) <= range) {
+					reflectMissile(m);
+				}
+			}
+		}
 	}
 
 	@Override
