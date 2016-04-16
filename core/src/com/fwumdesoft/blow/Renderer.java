9180e0452cc79633fbfd672ca55cf505c1febfc6
Ryan Goldstein <ryanisaacg@gmail.com>
2016-04-16 15:41:46 -0400

    Added initialization to gamescreen and renderer

--- core/src/com/fwumdesoft/blow/Renderer.java
+++ core/src/com/fwumdesoft/blow/Renderer.java
@@ -1,8 +1,17 @@
 package com.fwumdesoft.blow;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class Renderer {
-	
+	private SpriteBatch batch;
+
+	public Renderer(SpriteBatch batch) {
+		this.batch = batch;
+	}
+
 	public void render(GameScreen currentScreen) {
-		
+		batch.begin();
+		//TODO: DRAW HERE
+		batch.end();
 	}
 }
