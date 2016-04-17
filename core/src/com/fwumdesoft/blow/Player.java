91139dc7e221b01f1f074b57120f59344e044a34
Ryan Goldstein <ryanisaacg@gmail.com>
2016-04-16 22:10:27 -0400

    Fixed import warnings in the player object

--- core/src/com/fwumdesoft/blow/Player.java
+++ core/src/com/fwumdesoft/blow/Player.java
@@ -1,6 +1,5 @@
 package com.fwumdesoft.blow;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Intersector;
