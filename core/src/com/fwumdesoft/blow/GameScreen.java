0c28ecbf607fb21c50d29c21d4d572404bd41fd6
Ryan Goldstein <ryanisaacg@gmail.com>
2016-04-16 22:07:22 -0400

    Made progress towards the origin of the objects working

--- core/src/com/fwumdesoft/blow/GameScreen.java
+++ core/src/com/fwumdesoft/blow/GameScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Pool;
 import com.badlogic.gdx.utils.Pools;
@@ -33,6 +34,10 @@ public class GameScreen extends ScreenAdapter {
 		for(int i = 0; i < reflectors.length; i++) {
 			reflectors[i] = new Reflector(400, i); 
 			reflectors[i].setPosition(1920 / 2, 1080 / 2);
+			float angle = 45 * i; 
+			reflectors[i].setOrigin(0, 64);
+			reflectors[i].setX(reflectors[i].getX() + 100 * MathUtils.cosDeg(angle));
+			reflectors[i].setY(reflectors[i].getY() + 100 * MathUtils.sinDeg(angle));
 			stage.addActor(reflectors[i]);
 		}
 		stage.addActor(new InputManager(this, reflectors));
